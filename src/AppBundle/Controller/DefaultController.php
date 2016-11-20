8bad5963de03f10d43fd66a846133cecd90fd8ae
Carolina Romano <romano.carolina90@gmail.com>
2016-11-20 13:08:51 -0300

    Se agregaron los campos Nombre y Apellido al formulario de Usuarios y redirecciona index al login

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -13,9 +13,11 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
+        return $this->render('FOSUserBundle::Security/login.html.twig'); 
+
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', array(
-            'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
-        ));
+        //  return $this->render('default/index.html.twig', array(
+        //    'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+        //));
     }
 }
