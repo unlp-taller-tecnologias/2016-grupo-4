c44f5500f021cb7f851fab513aaee1a4ce4f6c57
maximilianomohamed <mohamedmaximiliano@gmail.com>
2016-12-14 22:21:42 -0300

    defaul controller modificado

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -1,11 +1,8 @@
 <?php
-
 namespace AppBundle\Controller;
-
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-
 class DefaultController extends Controller
 {
     /**
@@ -13,14 +10,21 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        return $this->render('FOSUserBundle::Security/login.html.twig', array(
+        if (false === $this->get('security.context')->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
+            $ruta = $this->generateUrl('fos_user_security_login');
+            return $this->redirect($ruta);
+        }else{
+            $ruta = $this->generateUrl('paciente_index');
+            return $this->redirect($ruta);
+            
+        }
+        /*return $this->render('FOSUserBundle::Security/login.html.twig', array(
             'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
         )); 
-
+*/
         // replace this example code with whatever you need
         //  return $this->render('default/index.html.twig', array(
         //    'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
         //));
     }
-
 }
